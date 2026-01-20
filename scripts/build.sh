5c14feeb310d74bc423524e17b169bd1a487d22b
wsjjason <2489170037@qq.com>
2026-01-20 10:29:21 +0800

    add version info in build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ set -ex
 
 mvn clean package
 cd target
-zip -r BoostKit-omniruntime-omniStateStore.zip flink-alg-falcon.jar
+zip -r BoostKit-omniruntime-omniStateStore-1.2.0.zip flink-alg-falcon.jar
