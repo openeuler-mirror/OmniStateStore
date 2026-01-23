81603b3198930873ce4c9c908cb1fd3b05b52e49
wjk20241 <callmetex@163.com>
2026-01-23 09:38:47 +0800

    定义静态成员变量

---
+++ src/core/common/util/key_group_util.cpp
@@ -0,0 +1,22 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include "key_group_util.h"
+
+namespace ock {
+namespace bss {
+
+// 定义静态成员变量
+KeyGroupUtil::SetKeyGroupFuncPtr KeyGroupUtil::mSetKeyGroupFunc = nullptr;
+KeyGroupUtil::ComputeKeyGroupFuncPtr KeyGroupUtil::mComputeKeyGroupFunc = nullptr;
+
+}  // namespace bss
+}  // namespace ock
